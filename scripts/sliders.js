0bf3a291e4ef999ad3d4034dabefb8ba04676852
n1kalai <47790698+n1kalai@users.noreply.github.com>
2019-05-30 18:33:14 +0400

    Add files via upload
    მთავარი გვერდის დასასრული

---
+++ scripts/sliders.js
@@ -0,0 +1,16 @@
+ $(document).ready(function() {
+            $('.slider').bxSlider();
+        });
+
+
+ $('.bookSlider').slick({
+          
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            centerMode: true, 
+            variableWidth: true,
+            infinite: true
+         
+                     
+
+        });
