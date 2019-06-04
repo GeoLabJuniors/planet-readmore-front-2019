ce04b2b4c479650f9121b6cbf6d93d9fd103a442
n1kalai <47790698+n1kalai@users.noreply.github.com>
2019-06-04 18:44:56 +0400

    Update sliders.js
    javascript update

--- scripts/sliders.js
+++ scripts/sliders.js
@@ -1,16 +1,29 @@
  $(document).ready(function() {
-            $('.slider').bxSlider();
+            $('.slider').bxSlider({
+				auto: true,
+				autoPlay: 5000,
+				speed: 1000,
+				 
+			});
         });
 
 
+$('.bxslider').bxSlider({
+  auto: true,
+  autoControls: true,
+  stopAutoOnClick: true,
+  pager: true,
+  slideWidth: 600
+});
+
  $('.bookSlider').slick({
-          
+            autoplay: true,
+	        autoplaySpeed: 3500,
             slidesToShow: 4,
             slidesToScroll: 1,
             centerMode: true, 
             variableWidth: true,
-            infinite: true
+            infinite: true,
+	        
          
-                     
-
-        });
+                             });
