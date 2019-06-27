87db263df16a1bb357df8173e1d382c6d0184aee
Nikoloz <47790698+n1kalai@users.noreply.github.com>
2019-06-27 14:44:45 +0400

    Add files via upload

---
+++ scripts/text_transformer.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  $("#flip").click(function(){
+    
+
+
+    $(".continue").slideToggle("slow"); 
+    $("#flip").css("transform","rotate(180deg)")
+     
+  });
+    
+});
+//Winners Text Transfromation 1
+$(document).ready(function(){
+  $("#flip2").click(function(){
+    
+
+
+    $(".winners_text").slideToggle("slow"); 
+    $("#flip2").css("transform","rotate(180deg)")
+     
+  });
+});
+//Winners Text Transfromation 3
+$(document).ready(function(){
+  $("#flip3").click(function(){
+    
+
+
+    $(".winners_text3").slideToggle("slow"); 
+    $("#flip3").css("transform","rotate(180deg)")
+     
+  });
+});
+
+//Winners Text Transfromation 4
+$(document).ready(function(){
+  $("#flip4").click(function(){
+    
+
+
+    $(".winners_text4").slideToggle("slow"); 
+    $("#flip4").css("transform","rotate(180deg)")
+     
+  });
+});
