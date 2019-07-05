769453a91f1f14f7cd1511eeff4a8b4eda4385f8
n1kalai <nikoloz.palagashvili@gmail.com>
2019-07-05 14:13:23 +0400

    x

---
+++ scripts/openModal.js
@@ -0,0 +1,8 @@
+function openModal () {
+    var modalSlider = document.getElementById('modalSlider');
+    var slider = document.getElementById('lastSlider');
+    modalSlider.style.display = "block";
+    modalSlider.innerHTML = slider;
+    
+
+    }
