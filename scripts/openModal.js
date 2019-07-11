6ad37decefb8d66d8f45743470445c799e81de71
n1kalai <nikoloz.palagashvili@gmail.com>
2019-07-11 21:48:53 +0400

    Modal Slider End

--- scripts/openModal.js
+++ scripts/openModal.js
@@ -1,6 +1,6 @@
 function openModal () {
     var modalSlider = document.getElementById('modalSlider');
-    var slider = document.getElementById('lastSlider');
+    var slider = document.getElementById('lastSlider').innerHTML;
     modalSlider.style.display = "block";
     modalSlider.innerHTML = slider;
     
