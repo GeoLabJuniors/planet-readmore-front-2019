1db03969890fb316e80cf3eb9c63764e6f6e6637
Nikoloz <47790698+n1kalai@users.noreply.github.com>
2019-09-19 15:05:08 +0400

    ss

--- scripts/openModal.js
+++ scripts/openModal.js
@@ -1,8 +1,15 @@
-function openModal () {
-    var modalSlider = document.getElementById('modalSlider');
-    var slider = document.getElementById('lastSlider').innerHTML;
-    modalSlider.style.display = "block";
-    modalSlider.innerHTML = slider;
-    
+let $login = document.querySelector('.openModal');
+let $Modal = document.querySelector('.mainModalPage');
+let $registModal = document.querySelector('.modalPage');
 
+$login.addEventListener('click', function (event) {
+    $Modal.style.display = "block";
+})
+
+
+window.onclick = function (event) {
+    console.log(event)
+    if (event.target == $Modal) {
+        $Modal.style.display = "none"
     }
+}
